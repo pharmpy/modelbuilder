c32e8ac69c762a1655b0d6a7fadc050a2c9124ce
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2023-10-19 16:02:20 +0200

    Only call one absorption or elimination function

--- callbacks/structural.py
+++ callbacks/structural.py
@@ -20,43 +20,42 @@ def structural_callbacks(app):
             Input("elim_radio", "value"),
             prevent_initial_call=True
     )
-
     def update_abs_elim(abs, elim):
         if elim:
-            elim_keys ={"FO": set_first_order_elimination(config.model),
-                    "MM": set_michaelis_menten_elimination(config.model),
-                    "mixed_MM_FO": set_mixed_mm_fo_elimination(config.model),
-                    "ZO" : set_zero_order_elimination(config.model)
+            elim_funcs ={"FO": set_first_order_elimination,
+                    "MM": set_michaelis_menten_elimination,
+                    "mixed_MM_FO": set_mixed_mm_fo_elimination,
+                    "ZO" : set_zero_order_elimination
                     }
-            
-            config.model = elim_keys[elim]
+
+            config.model = elim_funcs[elim](config.model)
             return True
         if abs:
-            abs_keys ={"ZO": set_zero_order_absorption(config.model),
-                    "FO": set_first_order_absorption(config.model),
-                    "seq_ZO_FO": set_seq_zo_fo_absorption(config.model)
+            abs_funcs = {"ZO": set_zero_order_absorption,
+                    "FO": set_first_order_absorption,
+                    "seq_ZO_FO": set_seq_zo_fo_absorption
                     }
-            config.model = abs_keys[abs]
+            config.model = abs_funcs[abs](config.model)
             return True
-        
+
     #Callback for disabeling absorption
     @app.callback(
             Output("abs_rate-radio", "options"),
             Output("abs_rate-radio", "value"),
             Input("route-radio", "value"),
             State("abs_rate-radio", "options"),
-            State("abs_rate-radio", "value"),       
+            State("abs_rate-radio", "value"),
     )
 
     def disable_abs(value, options, rate):
         if value == "iv":
             return [{**dictionary, "disabled":True} for dictionary in options], 0
-        else: 
+        else:
             return [{**dictionary, "disabled":False} for dictionary in options], rate
 
     @app.callback(
         [Output("lag-toggle", "options"),
-         Output("transit_input", "value"), 
+         Output("transit_input", "value"),
          Output("transit_input", "disabled")],
         Input("lag-toggle", "value"),
         Input("transit_input", "value"),
@@ -64,7 +63,7 @@ def structural_callbacks(app):
 )
 
     def toggle_disable(lag_tog, transit_input):
-        
+
         if lag_tog:
             try:
                 config.model = set_transit_compartments(config.model,0)
@@ -74,7 +73,7 @@ def structural_callbacks(app):
                 raise PreventUpdate
 
         if transit_input :
-    
+
             try:
                 config.model= remove_lag_time(config.model)
                 if transit_input:
@@ -82,16 +81,16 @@ def structural_callbacks(app):
                 return [{"label": "Lag Time", "value" : False, "disabled":True}], int(transit_input), False
             except:
 
-                raise PreventUpdate 
-        else: 
+                raise PreventUpdate
+        else:
             try:
                 config.model= remove_lag_time(config.model)
                 config.model = set_transit_compartments(config.model,0)
                 return [{"label": "Lag Time", "value" : True, "disabled":False}], None, False
             except:
                 return [{"label": "Lag Time", "value" : True, "disabled":False}], None, False
-    
-    
+
+
     #callback for peripheral compartments
     @app.callback(
         Output("data-dump", "clear_data", allow_duplicate=True),
@@ -109,7 +108,7 @@ def structural_callbacks(app):
             else:
                 while globals()["peripherals"] > 0:
                     config.model = remove_peripheral_compartment(config.model)
-                    globals()["peripherals"] = globals()["peripherals"] - 1 
+                    globals()["peripherals"] = globals()["peripherals"] - 1
         except: return True
 
     @app.callback(
@@ -123,6 +122,6 @@ def structural_callbacks(app):
             return True
         else:
             config.model = remove_bioavailability(config.model)
-            return True    
+            return True
 
     return
