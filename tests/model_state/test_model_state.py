24fb1dfe84619d810a06156060a0b02716cfed64
Stella <stella.belin@farmaci.uu.se>
2024-01-25 10:55:49 +0100

    Add more tests

--- tests/model_state/test_model_state.py
+++ tests/model_state/test_model_state.py
@@ -1,6 +1,6 @@
 from modelbuilder.internals.model_state import ModelState, create_model, update_model
 
-from pharmpy.modeling import has_instantaneous_absorption, has_first_order_absorption, has_first_order_elimination, create_basic_pk_model, has_michaelis_menten_elimination
+from pharmpy.modeling import has_zero_order_absorption, has_instantaneous_absorption, has_first_order_absorption, has_first_order_elimination, create_basic_pk_model, has_michaelis_menten_elimination
 
 
 def test_model_state_init():
@@ -23,7 +23,22 @@ def test_create_model():
 
 def test_update_model():
     model, model_state = create_model('iv')
+    assert has_first_order_elimination(model)
     model_new, model_state_new = update_model(model, model_state, 'ELIMINATION(MM)')
-    assert not has_first_order_elimination(model_new)
     assert has_michaelis_menten_elimination(model_new)
 
+    model, model_state = create_model('oral')
+    assert has_first_order_absorption(model)
+    model_new, model_state_new = update_model(model, model_state, 'ABSORPTION(ZO)')
+    assert has_zero_order_absorption(model_new)
+
+
+def test_update_model_nested():
+    model, model_state = create_model('iv')
+    assert has_first_order_elimination(model)
+    model_new, model_state_new = update_model(model, model_state, 'ELIMINATION(MM)')
+    assert has_michaelis_menten_elimination(model_new)
+    model_new, model_state_new = update_model(model_new, model_state_new, 'ELIMINATION(FO)')
+    assert has_first_order_elimination(model_new)
+    model_new, model_state_new = update_model(model_new, model_state_new, 'ELIMINATION(MM)')
+    assert has_michaelis_menten_elimination(model_new)
